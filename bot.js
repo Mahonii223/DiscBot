f28662a38357fcfaf158a66e42a0f17017a8f99b
Marcin Pham <marcin.kamo@gmail.com>
2020-12-18 00:24:17 +0100

    added help command

--- bot.js
+++ bot.js
@@ -7,12 +7,17 @@ const prefix = '.';
 const fs = require('fs'); 
 client.commands = new Discord.Collection();
 
+var validCommandsMap = new Map();
+
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command  = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
+    validCommandsMap.set(command.name, command.description);
 }
+
+validCommandsMap.delete("default");
 
 client.on('ready', () => {
 
@@ -29,18 +34,16 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    var validCommands = [
-        "ping", 
-        "rank",
-        "midget",
-        "choil",
-        "heagly",
-        "maker",
-        "moon",
-        "myne",
-    ];
-    if(validCommands.includes(command)){
+    
+    if(validCommandsMap.has(command)){
         client.commands.get(command).execute(message, args);
+    } else if (command == "help"){
+        var help = "Ugh, again? Here's the list of available commands, write it down somewhere and don't bother me again.\n";
+        for(let key of validCommandsMap.keys()){
+            var line = key + " : " + validCommandsMap.get(key) + "\n";
+            help = help + line;
+        }
+        message.channel.send(help);
     } else if(!command.startsWith(".") && !message === "."){
         client.commands.get("default").execute(message, args);
     }
