3a364997ef9827c5caeac87a0a80f9c8ec119de2
Marcin Pham <marcin.kamo@gmail.com>
2020-12-17 23:57:01 +0100

    emoji test

--- commands/choil.js
+++ commands/choil.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'choil',
     description: "politely asks for choil shot",
     execute(message, args){
-        message.channel.send('Alright, you know the rules. Put the choil shot in the bag and nobody get\'s hurt :gibchoil:');
+        message.channel.send('Alright, you know the rules. Put the choil shot in the bag and nobody get\'s hurt <:gibchoil:775110197651832832>');
     }
 }
