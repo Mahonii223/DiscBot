8a9afc287b237b6bd98639e77f76029fb6eb4716
Marcin Pham <marcin.kamo@gmail.com>
2020-12-17 20:35:28 +0100

    fixed incorrect imgur embed

--- commands/heagly.js
+++ commands/heagly.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'heagly',
     description: "heaglies a user",
     execute(message, args){
-        message.channel.send('https://imgur.com/a/CAoWFPi');
+        message.channel.send('https://i.imgur.com/EYdNCde.png');
     }
 }
