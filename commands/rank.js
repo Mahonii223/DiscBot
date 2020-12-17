e26dfa1ef73fca0c1ac01e0402359cc8ba337bb2
Marcin Pham <marcin.kamo@gmail.com>
2020-12-17 20:55:08 +0100

    changed rank wording

--- commands/rank.js
+++ commands/rank.js
@@ -3,12 +3,12 @@ module.exports = {
     description: "Provides your current rank",
     execute(message, args){
         var rankResponses = [
-            "Who cares? I don't. We have that other fella for that, but last time I've seen him he was getting a wedgie from Rhythm.", 
-            "Zero, as far as I'm considered.", 
-            "Hundred, what do I care? Whatever let's you sleep at night.",
-            "Maybe focus on your real world achievements for now, fella.",
-            "Well, you ain't the sharpest tool in the shed, let's put it that way.",
-            "Rank? What am I, some mee bitchboy?"];
+            "who cares? I don't. We have that other fella for that, but last time I've seen him he was getting a wedgie from Rhythm.", 
+            "your rank is zero, as far as I'm considered.", 
+            "I dunno, hundred, what do I care? Whatever let's you sleep at night.",
+            "maybe focus on your real world achievements for now, fella.",
+            "well, you ain't the sharpest tool in the shed, let's put it that way.",
+            "rank? What am I, some mee bitchboy?"];
         message.reply(rankResponses[Math.floor(Math.random() * rankResponses.length)]);
     }
 }
