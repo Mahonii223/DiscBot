47d5567659ad3bc5f5066c3e0d3827b11d1279d7
Marcin Pham <marcin.kamo@gmail.com>
2020-12-21 20:59:26 +0100

    fixed typo

--- commands/rank.js
+++ commands/rank.js
@@ -5,7 +5,7 @@ module.exports = {
         var rankResponses = [
             "who cares? I don't. We have that other fella for that, but last time I've seen him he was getting a wedgie from Rhythm.", 
             "your rank is zero, as far as I'm considered.", 
-            "I dunno, hundred, what do I care? Whatever let's you sleep at night.",
+            "I dunno, hundred, what do I care? Whatever lets you sleep at night.",
             "maybe focus on your real world achievements for now, fella.",
             "well, you ain't the sharpest tool in the shed, let's put it that way.",
             "rank? What am I, some mee bitchboy?",
