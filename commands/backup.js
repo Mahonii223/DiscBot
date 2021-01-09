6c8b911fe4b6d6ec9edfb4044a0d3b5e113e5ee2
Marcin Pham <marcin.kamo@gmail.com>
2021-01-10 00:44:03 +0100

    I say hidden

--- commands/backup.js
+++ commands/backup.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'backup',
     description: "displays all file-saved data",
+    visibility: "hidden",
+    perm: "moderator",
     execute(message, args){
         
         message.channel.send('Pong. this is getting boring.');
