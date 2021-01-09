2ad148186436b3e905b2b8e29563dd687af8141b
Marcin Pham <marcin.kamo@gmail.com>
2021-01-10 00:56:58 +0100

    arg test

--- commands/addpasta.js
+++ commands/addpasta.js
@@ -1,12 +1,16 @@
 module.exports = {
     name: 'addpasta',
     description: "saves the best memories",
+    perm: 'moderator',
     execute(message, args){
         var fs = require('fs');
-
+        
         fs.appendFile('copypastas.txt', 'Hello content!', function (err) {
             if (err) throw err;
-            message.channel.send('Pong. this is getting boring.');
         });
+
+        for(let arg of args){
+            message.channel.send("\narg: " + arg);
+        }
     }
 }
