8a55feae39b2cbb1366a733cf7fa3691507fb059
Marcin Pham <marcin.kamo@gmail.com>
2021-01-09 23:56:18 +0100

    pasta.js now reads the whole file

--- commands/pasta.js
+++ commands/pasta.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'pasta',
     description: "brings back the best memories",
     execute(message, args){
-        
+        var fs = require('fs')
+        fs.readFile('copypastas.txt', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        message.channel.send('data');
+});
     }
 }
