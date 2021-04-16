a72a7891640c6daf9610d6af536f5a56e1e8a78b
Marcin Pham <marcin.kamo@gmail.com>
2021-04-16 22:01:11 +0200

    testing

--- commands/v.js
+++ commands/v.js
@@ -7,7 +7,8 @@ module.exports = {
         
         //var channelMap = new Map();
         //channelMap.set(`#general`, `789170479026012162`);
-        const channel = message.client.channels.get(args.shift().substr(3, 18));
-        channel.send(args.join(' '));
+        //const channel = message.client.channels.get(args.shift().substr(3, 18));
+        //channel.send(args.join(' '));
+        message.channel.send(args.shift().substr(3, 18));
     }
 }
