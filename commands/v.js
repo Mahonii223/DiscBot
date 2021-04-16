507c6e497412ec29bb287d68af004ebd829e4aec
Marcin Pham <marcin.kamo@gmail.com>
2021-04-16 21:17:34 +0200

    testing

--- commands/v.js
+++ commands/v.js
@@ -4,8 +4,8 @@ module.exports = {
     visibility: "hidden",
     enabled: true,
     execute(message, args){
-        message.channel.send(message.client.channels.toString());
-        //const channel = message.client.channels.cache.find(channel => channel.name === args.shift());
-        //channel.send(args);
+        //message.channel.send(message.client.channels.toString());
+        const channel = message.client.channels.cache.find(channel => channel.name === args.shift());
+        channel.send(args);
     }
 }
