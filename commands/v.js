d772434780486019a20840174038fbf269fe6b8c
Marcin Pham <marcin.kamo@gmail.com>
2021-04-16 22:06:06 +0200

    testing

--- commands/v.js
+++ commands/v.js
@@ -4,11 +4,8 @@ module.exports = {
     visibility: "hidden",
     enabled: true,
     execute(message, args){
-        
-        //var channelMap = new Map();
-        //channelMap.set(`#general`, `789170479026012162`);
-        //const channel = message.client.channels.get(args.shift().substr(3, 18));
-        //channel.send(args.join(' '));
-        message.reply(args.shift().substr(3, 18));
+        const channel = message.client.channels.get(args.shift().substr(2, 18));
+        channel.send(args.join(' '));
+        //message.reply(args.shift().substr(2, 18));
     }
 }
