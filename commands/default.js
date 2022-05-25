47064cdb51cdc0fd6098ada896c0cdc32a4ad1e7
Marcin Pham <marcin.kamo@gmail.com>
2022-05-25 21:43:00 +0200

    added randomized insult on default

--- commands/default.js
+++ commands/default.js
@@ -1,9 +1,13 @@
+import { getShInsult } from utils/shakespeareanInsults.js
+
+
 module.exports = {
     name: 'default',
     description: "default command",
     visibility: "hidden",
     enabled: true,
     execute(message, args){
-        message.reply("are you simple? \"" + message + "\" is not a valid command, you cabbage.");
+        var insult = getShInsult();
+        message.reply("are you simple? \"" + message + "\" is not a valid command, you " + insult + ".");
     }
 }
